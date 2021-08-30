4132c408c091ac072ae559a0f57dec2fde0211f8
pengsheng.zhu <1137846786@qq.com>
2021-08-30 13:50:39 +0800

    init

---
+++ src/router/router.js
@@ -0,0 +1,29 @@
+import Home from '../pages/home.vue'
+import List from '../pages/list.vue'
+import Details from '../pages/details.vue'
+import VueRouter from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/list',
+    component: List
+  },
+  {
+    path: '/details',
+    component: Details
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
